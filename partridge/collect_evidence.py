cafed99066c353b52f7d0a390c1422bf11e894d8
niekwit <niekwit@gmail.com>
2026-07-05 17:57:43 +0100

    added CLI, and black formatted

--- partridge/collect_evidence.py
+++ partridge/collect_evidence.py
@@ -1,27 +1,45 @@
-
-# run with singularity
-# singularity exec --bind bam_slx26173:/input --bind slx26173_evidence:/output --bind slx26173_insertions.xlsx:/input_excel.xlsx --bind collect_evidence.py:/collect_evidence.py /home/jd862/rds/rds-sjl83-green/jd862/py13_pandas_pysam.sif python /collect_evidence.py $b
+import argparse
+import gzip
+import os
 
 import pandas as pd
 from pysam import AlignmentFile, CSOFT_CLIP
-import gzip
-import sys, os
 
-args = sys.argv
+parser = argparse.ArgumentParser(
+    prog="collect_evidence.py",
+    description=(
+        "Gather evidence sequences and paired-mate reads flanking each "
+        "candidate insertion breakpoint, for manual verification."
+    ),
+)
+parser.add_argument(
+    "--audit-xlsx",
+    required=True,
+    help="Path to isa.audit.xlsx (from collect_insertions.R)",
+)
+parser.add_argument("--bam", required=True, help="Path to the bam file for this sample")
+parser.add_argument("--output", required=True, help="Path to output fa.gz file")
+parser.add_argument(
+    "--sample",
+    required=True,
+    help="Sample identifier to filter the audit table's 'mouse' column by",
+)
+args = parser.parse_args()
 
-PATH_TO_AUDIT = 'test_data/isa.audit.xlsx'
-PATH_TO_OUTPUT = f'test_data/{sys.argv[1]}.fa.gz'
-d = pd.read_excel(PATH_TO_AUDIT)
-PATH_TO_BAM = None
-for f in os.listdir("test_data"):
-    if sys.argv[1] in f:
-        if f.endswith(".bam"):
-            PATH_TO_BAM = "test_data/"+f
-if PATH_TO_BAM is None:
-    print(f"BAM NOT FOUND for index {sys.argv[1]}")
+PATH_TO_AUDIT = args.audit_xlsx
+PATH_TO_BAM = args.bam
+PATH_TO_OUTPUT = args.output
+SAMPLE = args.sample
+
+if not os.path.isfile(PATH_TO_BAM):
+    print(f"ERROR: BAM file not found: {PATH_TO_BAM}")
     exit(1)
 
+d = pd.read_excel(PATH_TO_AUDIT)
+
 reads = {}
+
+
 class Breakpoint:
     def __init__(self, seqname, pos1, pos2, strand):
         self.seqname = seqname
@@ -47,27 +65,28 @@ class Breakpoint:
         # make sure alrady used mates are not used again.
         self.left_mates = self.left_mates.difference(self.covered_fwd)
         self.right_mates = self.right_mates.difference(self.covered_rev)
-        self.covered_fwd = None # remove
-        self.covered_rev = None #remove
+        self.covered_fwd = None  # remove
+        self.covered_rev = None  # remove
         self.left_clip = sorted(self.left_clip, key=lambda x: len(x))
         self.right_clip = sorted(self.right_clip, key=lambda x: len(x))
 
     def __str__(self):
         return f"{self.seqname}:{self.pos1}-{self.pos2} ({self.strand})"
 
-with AlignmentFile(PATH_TO_BAM, mode='rb') as af:
+
+with AlignmentFile(PATH_TO_BAM, mode="rb") as af:
     bps = []
-    for i, (x, strand) in d[d['mouse']==sys.argv[1]][['name', "strand"]].iterrows():
+    for i, (x, strand) in d[d["mouse"] == SAMPLE][["name", "strand"]].iterrows():
         seqname, pos = x.split(":")
         pos = int(pos)
         print(f"Processing {seqname}:{pos}")
-        bp = Breakpoint(seqname, pos, pos+6,strand)
+        bp = Breakpoint(seqname, pos, pos + 6, strand)
         print(str(bp))
-        for r in af.fetch(seqname, pos-1, pos+7):
+        for r in af.fetch(seqname, pos - 1, pos + 7):
             p = 0
             lclip = 0
             rclip = 0
-            for (m, l) in r.cigartuples:
+            for m, l in r.cigartuples:
                 if m == CSOFT_CLIP:
                     if p == 0:
                         lclip = l
@@ -76,8 +95,10 @@ with AlignmentFile(PATH_TO_BAM, mode='rb') as af:
                 else:
                     rclip = 0
                 p += l
-            if lclip and rclip: continue
-            if not lclip and not rclip: continue
+            if lclip and rclip:
+                continue
+            if not lclip and not rclip:
+                continue
             ### reads are always mapped to + strand
             ### if read1 is not reversed, read 2 is to the right, read1 ^ is_forward = 1 ^ 1 = 0
             #    r1 ---->   <----- r2
@@ -93,7 +114,9 @@ with AlignmentFile(PATH_TO_BAM, mode='rb') as af:
                 if r.is_reverse:
                     bp.left_mates.add(r.query_name)
             if rclip:
-                bp.add_right_clip(r.query_sequence[(len(r.query_sequence)-rclip):], r.query_name)
+                bp.add_right_clip(
+                    r.query_sequence[(len(r.query_sequence) - rclip) :], r.query_name
+                )
                 if r.is_forward:
                     bp.right_mates.add(r.query_name)
 
@@ -104,7 +127,9 @@ with AlignmentFile(PATH_TO_BAM, mode='rb') as af:
                 bp.covered_rev.add(r.query_name)
         bp.cleanup()
         bps.append(bp)
-        print(f"missing {len(bp.left_mates)} left-mates and {len(bp.right_mates)} right-mates")
+        print(
+            f"missing {len(bp.left_mates)} left-mates and {len(bp.right_mates)} right-mates"
+        )
     print(f"finding mates")
     af.reset()
     for r in af:
@@ -118,7 +143,7 @@ with AlignmentFile(PATH_TO_BAM, mode='rb') as af:
                 if r.query_name in bp.left_mates:
                     bp.left_mate_seq.append(r.query_sequence)
                     bp.left_mates.remove(r.query_name)
-    with gzip.open(PATH_TO_OUTPUT, 'wt') as opt:
+    with gzip.open(PATH_TO_OUTPUT, "wt") as opt:
         for bp in bps:
             opt.write(f"@{str(bp)}\n")
             opt.write(f">RIGHT_CLIP\n")
@@ -134,6 +159,3 @@ with AlignmentFile(PATH_TO_BAM, mode='rb') as af:
             opt.write(f">RIGHT_MATE\n")
             for seq in bp.right_mate_seq:
                 opt.write(f"{seq}\n")
-
-
-
